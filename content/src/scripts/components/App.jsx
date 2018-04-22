5597d2fc3260a8c76f6e54aabf4beaa934f46938
DinexNsk <dinex.nsk@gmail.com>
2018-04-22 19:13:38 +0700

    added sendMessage. when message is closed

--- content/src/scripts/components/App.jsx
+++ content/src/scripts/components/App.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable-rule no-undef */
- /*global chrome*/
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import '../../css/styles.css';
@@ -12,7 +10,17 @@ class App extends Component {
 
   remove (){
     let item = document.getElementById('rcr-anchor');
+
     document.documentElement.removeChild(item);
+    chrome.runtime.sendMessage({messageText: 3}, this.getResponse);
+  }
+
+  getResponse(response) {
+    if (chrome.runtime.lastError) {
+      console.log("ERROR: ", chrome.runtime.lastError);
+    } else {
+      console.log(response);  
+    }
   }
 
   render() {
